c898b2bc18d15243d0e04f313961da1887a9ab9b
hk_117 <zubayer11704@gmail.com>
2024-01-29 23:33:41 +0600

    Banking application initialized

---
+++ src/main/java/com/zubayer/bankingapp/BankingApp.java
@@ -0,0 +1,13 @@
+package com.zubayer.bankingapp;
+
+/**
+ *
+ * @author Md. Zubayer Islam
+ */
+
+public class BankingApp {
+
+    public static void main(String[] args) {
+        System.out.println("Banking Application Initial");
+    }
+}
