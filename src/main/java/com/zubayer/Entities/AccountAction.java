4fbb75c4bfc65637771e568d18faa1981fb206f0
hk_117 <zubayer11704@gmail.com>
2024-01-31 00:35:39 +0600

    Basic UI added

--- src/main/java/com/zubayer/Entities/AccountAction.java
+++ src/main/java/com/zubayer/Entities/AccountAction.java
@@ -5,6 +5,10 @@ package com.zubayer.Entities;
  * @author Md. Zubayer Islam
  */
 public interface AccountAction {
+    /*
+        Different accounts may have these 
+        functions common with different implementation
+    */
     public void deposit(double amount);
     public boolean withdraw(double amount);
 }
---
+++ src/main/java/com/zubayer/UI/ApplicationUI.java
@@ -0,0 +1,54 @@
+package com.zubayer.UI;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Md. Zubayer Islam
+ */
+public class ApplicationUI {
+    
+    public String[] MAIN_MENU = {
+        "Create a new account",
+        "Display all account",
+        "Update an account",
+        "Delete an account",
+        "Deposit an amount",
+        "Withdraw an amount",
+        "Search",
+        "Exit"
+    };
+    
+    public String[] ACCOUNT_TYPE_MENU = {
+        "Current Account",
+        "Savings Account",
+        "Salary Account",
+        "Business Account"
+    };
+    
+    public void welcome(){
+        System.out.println("--------------------------------------------");
+        System.out.println("|        Welcome to Banking App            |");
+        System.out.println("--------------------------------------------\n");
+    }
+    
+    public void printSeparator(){
+        System.out.println("--------------------------------------------\n");
+    }
+    
+    public int ShowMenu(String[] menu){
+        for(int i=0; i<menu.length; i++){
+            System.out.println((i+1) + ". " + menu[i] + "\n");
+        }
+        System.out.println("Choose your option: ");
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do{
+            choice = sc.nextInt();
+            if(choice < 1 || choice> menu.length){
+                System.out.println("Invalid option!, Please try again: ");
+            }
+        } while (choice <1 || choice> menu.length);
+        return choice;
+    }
+}
